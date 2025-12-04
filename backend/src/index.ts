21eee95fc48c53df99b7d6a77ece67b32d2a6bbb
Pranaysakpal18 <pranay.sakpal18@gmail.com>
2025-12-04 16:08:14 +0530

    Day 2: Backend setup with Node, Express, TS

---
+++ backend/src/index.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/ping";
+
+
+dotenv.config();  //.env variable load karta hai 
+
+
+
+const app=express();
+
+
+
+//agar .env me PORT set hai use karo nahi to default 5000 port use karo
+const PORT =process.env.PORT || 5000;
+
+
+
+//middleware setup
+//1.cors 
+app.use(cors());
+//2.backend JSON request body parse karta hai 
+app.use(express.json());
+
+
+//routes
+app.use("/ping",router);
+
+
+//start server
+app.listen(PORT,()=>{
+    console.log(`server running on http://localhost:${PORT}`)
+});
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Backend!");
+});
